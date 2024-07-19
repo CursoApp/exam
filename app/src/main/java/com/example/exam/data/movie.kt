98b74e478b16ab5d38fc34358227ea22b9c000b4
YO <ojara@gmx.com>
2024-07-19 12:09:38 +0200

    exma viernes 3

--- app/src/main/java/com/example/exam/data/movie.kt
+++ app/src/main/java/com/example/exam/data/movie.kt
@@ -1,17 +1,19 @@
 package com.example.exam.data
 
+import android.media.Image
 import com.google.gson.annotations.SerializedName
 
 data class Movie(
     @SerializedName("imdbID") val id: Int,
     @SerializedName("Title") val title: String,
-    @SerializedName("Released") val release_date: String,
-    @SerializedName("Poster") val posterPath: String,
-    @SerializedName("Plot") val overview: String,
+    @SerializedName("Year") val year: String,
+    @SerializedName("Type") val type: String,
+    @SerializedName("Poster") val image: String,
+    @SerializedName("Plot") val plot: String,
     @SerializedName("Runtime") val runtime: String,
     @SerializedName("Director") val director: String,
-    @SerializedName("Genre") val genres: String,
-    @SerializedName("country") val country: String,
+    @SerializedName("Genre") val genre: String,
+    @SerializedName("Country") val country: String,
 )
 
 //Título
